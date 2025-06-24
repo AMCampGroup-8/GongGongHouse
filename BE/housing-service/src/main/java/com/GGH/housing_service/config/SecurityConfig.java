30bd525f67b757aa1a2328181db0ebfec4c1e427
mz <ieecno22@gmail.com>
2025-06-24 23:59:47 +0900

    Feat: JWT 인증 검증 추가

---
+++ BE/housing-service/src/main/java/com/GGH/housing_service/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.GGH.housing_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+    this.jwtTokenProvider = jwtTokenProvider;
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+        .httpBasic(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/housing/**").authenticated()
+            .anyRequest().permitAll()
+        )
+        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+            UsernamePasswordAuthenticationFilter.class)
+        .build();
+  }
+}
