6563f576fa219eaed8bbd3d56b9e98f9ed10eafc
Tndals61 <sungmin.jeon.rafael@gmail.com>
2025-06-26 02:57:49 +0900

    Revert "폴더 이름 바꿈"
    This reverts commit 25d0c2cbdf7cb4050c8d5041853c5c0918085e7f.

---
+++ FE/notification-service/tailwind.config.cjs
@@ -0,0 +1,15 @@
+// tailwind.config.cjs
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'media', // 또는 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
